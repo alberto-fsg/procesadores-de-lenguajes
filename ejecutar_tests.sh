a2cb2e084a87c71e8ea9f63d5205dc9131a8e52d
Alberto Francisco Solaz García <albertofranciscosolazgarcia@gmail.com>
2025-02-12 16:05:44 +0100

    Corregir error ejecutar_tests.sh

--- ejecutar_tests.sh
+++ ejecutar_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 RUTA_TEST="test"
-RUTA_ANALIZADOR_LEXICO="practica_1_esqueleto_proyecto/dist"
+RUTA_ANALIZADOR_LEXICO="practica1/dist"
 
 # Compilar el proyecto
 echo -e "\033[0;33mCompilando el proyecto\033[0m\n"
