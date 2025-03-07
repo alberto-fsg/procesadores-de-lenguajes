b6dce17581f73314e5ad04503d30fd3db128b1f6
Alberto Francisco Solaz García <albertofranciscosolazgarcia@gmail.com>
2025-03-07 13:31:44 +0100

    Devolver a la vida a /lib/symbolTable/AtribExp.java

--- practica3/lib/symbolTable/AtribExp.java
+++ practica3/lib/symbolTable/AtribExp.java
@@ -2,51 +2,18 @@ package lib.symbolTable;
 
 public class AtribExp {
     public Symbol.Types type;
-    public String op;
-    public AtribExp left;
-    public AtribExp right;
-    public String value;
-    public boolean isConst;
-    public boolean isLiteral;
+    public String image;
+    public Boolean isVar;
+    public Boolean isConst;
 
-    // Constantes
-    public AtribExp(Symbol.Types type, String value, boolean isConst, boolean isLiteral) {
-        this.type = type;
-        this.value = value;
-        this.isConst = isConst;
-        this.isLiteral = isLiteral;
-        this.op = null;
-        this.left = null;
-        this.right = null;
+    public AtribExp() {
+        type = Symbol.Types.UNDEFINED;
     }
 
-    // Expresión
-    public AtribExp(Symbol.Types type, String op, AtribExp left, AtribExp right, boolean isConst) {
-        this.type = type;
-        this.op = op;
-        this.left = left;
-        this.right = right;
-        this.isConst = isConst;
-        this.isLiteral = false;
-        this.value = null;
-    }
-
-    // Evauluar expresiones en tiempo de compilación
-    public boolean evaluate() throws RuntimeException {
-        /*
-        if (isConstant) {
-            return Boolean.parseBoolean(value);
-        }
-        if (op.equals("&&")) {
-            return left.evaluate() && right.evaluate();
-        } else if (op.equals("||")) {
-            return left.evaluate() || right.evaluate();
-        }
-        throw new RuntimeException("Unknown operator: " + op);
-        */
-
-        switch (this.op) {
-            case ("&")
-        }
+    public AtribExp(Symbol.Types type, String image, Boolean isVar, Boolean isConst) {
+            this.type = type;
+            this.image = image;
+            this.isVar = isVar;
+            this.isConst = isConst;
     }
 }
