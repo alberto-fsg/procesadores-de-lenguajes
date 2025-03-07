189a97b0700dd327d9aefbfd75a5410de1b63e5e
Alberto Francisco Solaz García <albertofranciscosolazgarcia@gmail.com>
2025-03-07 10:53:43 +0100

    Parte a medias del análisis semántico

--- practica3/lib/symbolTable/AtribExp.java
+++ practica3/lib/symbolTable/AtribExp.java
@@ -2,14 +2,51 @@ package lib.symbolTable;
 
 public class AtribExp {
     public Symbol.Types type;
-    public String image;
+    public String op;
+    public AtribExp left;
+    public AtribExp right;
+    public String value;
+    public boolean isConst;
+    public boolean isLiteral;
 
-    public AtribExp() {
-        type = Symbol.Types.UNDEFINED;
+    // Constantes
+    public AtribExp(Symbol.Types type, String value, boolean isConst, boolean isLiteral) {
+        this.type = type;
+        this.value = value;
+        this.isConst = isConst;
+        this.isLiteral = isLiteral;
+        this.op = null;
+        this.left = null;
+        this.right = null;
     }
 
-    public AtribExp(Symbol.Types type, String image) {
-            this.type = type;
-            this.image = image;
+    // Expresión
+    public AtribExp(Symbol.Types type, String op, AtribExp left, AtribExp right, boolean isConst) {
+        this.type = type;
+        this.op = op;
+        this.left = left;
+        this.right = right;
+        this.isConst = isConst;
+        this.isLiteral = false;
+        this.value = null;
+    }
+
+    // Evauluar expresiones en tiempo de compilación
+    public boolean evaluate() throws RuntimeException {
+        /*
+        if (isConstant) {
+            return Boolean.parseBoolean(value);
+        }
+        if (op.equals("&&")) {
+            return left.evaluate() && right.evaluate();
+        } else if (op.equals("||")) {
+            return left.evaluate() || right.evaluate();
+        }
+        throw new RuntimeException("Unknown operator: " + op);
+        */
+
+        switch (this.op) {
+            case ("&")
+        }
     }
 }
---
+++ practica3/lib/symbolTable/exceptions/TypesDontMatchException.java
@@ -0,0 +1,9 @@
+package lib.symbolTable.exceptions;
+
+public class TypesDontMatchException extends Error {
+
+	public TypesDontMatchException() {
+
+	}
+
+}
