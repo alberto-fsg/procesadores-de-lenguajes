aecb318772ee9b368075fe1147b82e5fb4464d8d
Alberto Francisco Solaz García <albertofranciscosolazgarcia@gmail.com>
2025-03-09 11:04:08 +0100

    Comentar build.sh modularizado

--- build.sh
+++ build.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-cd ./practica3/traductor/modulos
-cat main.jj tokens.jj programa.jj tipos.jj variables.jj expresiones.jj funciones.jj instrucciones.jj > gcl.jj
-mv gcl.jj ..
-cd ../../..
+INPUT_DIR="./practica3/traductor/modulos"
+OUTPUT_FILE="./practica3/traductor/gcl.jj"
 
+# si los directorios no existen abortar en lugar de estamparse
+if [ ! -d "$INPUT_DIR" ]; then
+    echo "Error: El directorio $INPUT_DIR no existe."
+    exit 1
+fi
+
+# concatenar a mano todos los ficheros porque la versión de Java es prehistórica :D
+cat "$INPUT_DIR/main.jj" "$INPUT_DIR/tokens.jj" "$INPUT_DIR/programa.jj" \
+    "$INPUT_DIR/tipos.jj" "$INPUT_DIR/variables.jj" "$INPUT_DIR/expresiones.jj" \
+    "$INPUT_DIR/funciones.jj" "$INPUT_DIR/instrucciones.jj" > "$INPUT_DIR"
+
+# compilar con Apache Ant
 ant -f practica3
