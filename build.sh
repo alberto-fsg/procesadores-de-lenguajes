6c056b908ff1042e9b3950c19d4650ca53205364
Alberto Francisco Solaz García <albertofranciscosolazgarcia@gmail.com>
2025-03-09 10:57:21 +0100

    Modularizar traductor

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+cd ./practica3/traductor/modulos
+cat main.jj tokens.jj programa.jj tipos.jj variables.jj expresiones.jj funciones.jj instrucciones.jj > gcl.jj
+mv gcl.jj ..
+cd ../../..
+
 ant -f practica3
